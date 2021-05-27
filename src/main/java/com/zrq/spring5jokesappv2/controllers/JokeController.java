5014fa895c7144d95008f740525633b8fe939bf8
Ricardo Quintas <rdquintas@yahoo.com>
2021-05-27 23:12:14 +0100

    adding main files

---
+++ src/main/java/com/zrq/spring5jokesappv2/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package com.zrq.spring5jokesappv2.controllers;
+
+import com.zrq.spring5jokesappv2.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
---
+++ src/main/java/com/zrq/spring5jokesappv2/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.zrq.spring5jokesappv2.services;
+
+public interface JokeService {
+    String getJoke();
+}
